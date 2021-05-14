1f0ed550c6da68e74065189dc62e93ee60161155
IkyAmril <a2472320@bangkit.academy>
2021-05-14 22:37:09 +0800

    add greetings and layout

---
+++ app/src/main/java/com/khairul/bisindosignlanguangerecognition/NumberActivity.kt
@@ -0,0 +1,11 @@
+package com.khairul.bisindosignlanguangerecognition
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NumberActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_number)
+    }
+}
