4b3b9f4a69a1e65043db2e64f67f5a66cf8ffb49
Milky-way <79302158+khairul3@users.noreply.github.com>
2021-05-23 20:22:12 +0800

    modify many things

---
+++ app/src/main/java/com/khairul/bisindosignlanguangerecognition/ui/dictionary/DictionaryFragment.kt
@@ -0,0 +1,24 @@
+package com.khairul.bisindosignlanguangerecognition.ui.dictionary
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.khairul.bisindosignlanguangerecognition.R
+
+class DictionaryFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dictionary, container, false)
+    }}
