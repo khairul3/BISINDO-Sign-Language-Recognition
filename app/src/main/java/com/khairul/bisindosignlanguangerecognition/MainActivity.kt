7aed9ed994a50b73b058370aa35aa18adcb42942
Milky-way <79302158+khairul3@users.noreply.github.com>
2021-05-25 20:58:39 +0800

    change layout

--- app/src/main/java/com/khairul/bisindosignlanguangerecognition/MainActivity.kt
+++ app/src/main/java/com/khairul/bisindosignlanguangerecognition/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.khairul.bisindosignlanguangerecognition
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
