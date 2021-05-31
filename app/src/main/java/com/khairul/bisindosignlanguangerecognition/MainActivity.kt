ed55c8d0b009f2a5c9b990a47447920f81eead1d
IkyAmril <a2472320@bangkit.academy>
2021-05-31 13:56:03 +0800

    add intent

--- app/src/main/java/com/khairul/bisindosignlanguangerecognition/MainActivity.kt
+++ app/src/main/java/com/khairul/bisindosignlanguangerecognition/MainActivity.kt
@@ -1,11 +1,63 @@
 package com.khairul.bisindosignlanguangerecognition
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.khairul.bisindosignlanguangerecognition.databinding.ActivityMain1Binding
+import com.khairul.bisindosignlanguangerecognition.ui.CameraFragment
+import com.khairul.bisindosignlanguangerecognition.ui.dictionary.DictionaryFragment
+import com.khairul.bisindosignlanguangerecognition.ui.number.NumberFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMain1Binding
+
+    private val onNavigationItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> MainActivity()
+                R.id.nav_camera -> loadFragment(CameraFragment())
+            }
+            true
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main1)
+        val navigationView: BottomNavigationView = findViewById(R.id.bottom_navigation_view)
+        navigationView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+        binding.imgCam.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(intent)
+        }
+
+        binding.btnDictionary.setOnClickListener {
+            val fragment: Fragment = DictionaryFragment()
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.rv_dic, fragment)
+                .commit()
+        }
+
+        binding.btnNumber.setOnClickListener {
+            val fragment: Fragment = NumberFragment()
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.rv_number, fragment)
+                .commit()
+        }
+
+    }
+
+    private fun loadFragment(fragment: Fragment?): Boolean {
+        if (fragment != null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit()
+            return true
+        }
+        return false
     }
 }
---
+++ app/src/main/java/com/khairul/bisindosignlanguangerecognition/ui/CameraFragment.kt
@@ -0,0 +1,24 @@
+package com.khairul.bisindosignlanguangerecognition.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.khairul.bisindosignlanguangerecognition.R
+
+class CameraFragment : Fragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_camera, container, false)
+    }
+
+}
