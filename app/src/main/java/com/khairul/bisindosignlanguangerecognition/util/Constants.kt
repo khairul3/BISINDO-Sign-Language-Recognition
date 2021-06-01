b74c04808200877ded2a5e942bad5373336c1ad2
Milky-way <79302158+khairul3@users.noreply.github.com>
2021-06-01 20:21:12 +0800

    change

--- app/src/main/java/com/khairul/bisindosignlanguangerecognition/util/Constants.kt
+++ app/src/main/java/com/khairul/bisindosignlanguangerecognition/util/Constants.kt
@@ -3,7 +3,6 @@ package com.khairul.bisindosignlanguangerecognition.util
 object Constants {
     val ALPHABET = "alfabet"
     val NUMBER = "angka"
-    val sort = "label"
 
 
     const val COLLECTION_POST = "posts"
